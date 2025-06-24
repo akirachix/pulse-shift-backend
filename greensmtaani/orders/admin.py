abc4f9d902296a8101d18ff57c5df09f8ac8a733
lwam <lwambsrat@gmail.com>
2025-06-24 15:22:45 +0300

    update code

--- greensmtaani/orders/admin.py
+++ greensmtaani/orders/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Orders, Orders_item
 
 admin.site.register(Orders)
-admin.site.register(Orders_item)
+admin.site.register(Order_items)
 
 # Register your models here.
