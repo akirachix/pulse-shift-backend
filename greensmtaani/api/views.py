8394fb0fedbc72f3530e9680fceb0df996e0461d
yordanoshagos <yordanostesfayhagos@gmail.com>
2025-06-25 11:47:19 +0300

    add products app API

--- greensmtaani/api/views.py
+++ greensmtaani/api/views.py
@@ -1,39 +1,9 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from users.models import Customer, MamaMboga, Address
-from orders.models import Orders, Order_items
-from nutrition.models import DietaryPreference,MealPlan
 from products.models import Product, ProductCategory, StockRecord
-from .serializer import CustomerSerializer, MamaMbogaSerializer, AddressSerializer, Order_itemsSerializer, OrdersSerializer, ProductSerializer, ProductCategorySerializer, StockRecordSerializer,DietaryPreferenceSerializer,MealPlanSerializer
+from .serializer import ProductSerializer, ProductCategorySerializer, StockRecordSerializer
 
 # Create your views here.
-class CustomerViewSet(viewsets.ModelViewSet):
-    queryset = Customer.objects.all()
-    serializer_class = CustomerSerializer
-
-class MamaMbogaViewSet(viewsets.ModelViewSet):
-    queryset = MamaMboga.objects.all()
-    serializer_class = MamaMbogaSerializer
-
-class AddressViewSet(viewsets.ModelViewSet):
-    queryset = Address.objects.all()
-    serializer_class = AddressSerializer
-
-class OrdersViewSet(viewsets.ModelViewSet):
-    queryset = Orders.objects.all()
-    serializer_class = OrdersSerializer
-
-class Order_itemsViewSet(viewsets.ModelViewSet):
-    queryset = Order_items.objects.all()
-    serializer_class =Order_itemsSerializer
-
-class DietaryPreferenceViewSet(viewsets.ModelViewSet):
-    queryset = DietaryPreference.objects.all()
-    serializer_class = DietaryPreferenceSerializer
-
-class MealPlanViewSet(viewsets.ModelViewSet):
-    queryset = MealPlan.objects.all()
-    serializer_class = MealPlanSerializer
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
