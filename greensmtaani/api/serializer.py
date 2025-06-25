21e0240be3475f7bcf520e3c3b7861bd01e2819c
Danait Semere <semeregdanait@gmail.com>
2025-06-25 12:19:27 +0300

    edit file

--- greensmtaani/api/serializer.py
+++ greensmtaani/api/serializer.py
@@ -1,55 +1,16 @@
 from rest_framework import serializers
-from users.models import Customer, MamaMboga, Address
-from orders.models import Orders, Order_items
-from nutrition.models import DietaryPreference,MealPlan
-from products.models import Product, ProductCategory, StockRecord 
+
+from payments.models import Transaction
 
 # users APIs
-class CustomerSerializer(serializers.ModelSerializer):
+class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Customer
+        model = Transaction
         fields = '__all__'
 
-class MamaMbogaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MamaMboga
-        fields = '__all__'
 
-class AddressSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Address
-        fields = '__all__'
 
-class OrdersSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Orders
-        fields = '__all__'
-        
-class Order_itemsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order_items
-        fields = '__all__'
 
-class DietaryPreferenceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = DietaryPreference
-        fields = '__all__'
-class MealPlanSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MealPlan
-        fields = '__all__'        
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = '__all__'
-class ProductCategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ProductCategory
-        fields = '__all__'
-class StockRecordSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = StockRecord
-        fields = '__all__'
 
 
 
--- greensmtaani/users/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Customer, MamaMboga, Address
-
-admin.site.register(Customer)
-admin.site.register(MamaMboga)
-admin.site.register(Address)
