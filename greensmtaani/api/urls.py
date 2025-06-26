69aeefa2eafe0052904d60d395d5b9c5f0d02fec
Fiona Wesonga <fionahwesonga@gmail.com>
2025-06-26 19:03:07 +0300

    Resolve  conflict in the api app

--- greensmtaani/api/urls.py
+++ greensmtaani/api/urls.py
@@ -1,21 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
-ProductViewSet, ProductCategoryViewSet, StockRecordViewSet,
-    DietaryPreferenceViewSet, MealPlanViewSet,
-    OrdersViewSet, Order_itemsViewSet,TransactionViewSet )
+    DietaryPreferenceViewSet, MealPlanViewSet )
    
 
 router = DefaultRouter()
 router.register(r"dietary-preferences", DietaryPreferenceViewSet, basename='dietary-preferences')
 router.register(r"meal-plans", MealPlanViewSet, basename='meal-plans')
-router.register(r"orders", OrdersViewSet, basename='orders')
-router.register(r"order-items", Order_itemsViewSet, basename='order-items')
-router.register(r"products", ProductViewSet, basename='products')
-router.register(r"product_catagory",ProductCategoryViewSet, basename='product_catagory')
-router.register(r"stock_record",StockRecordViewSet, basename='stock_record')
-router.register(r"transaction",TransactionViewSet, basename='transaction')
-
 urlpatterns = [
     path('', include(router.urls)),
 ]
--- greensmtaani/api/views.py
+++ greensmtaani/api/views.py
@@ -1,13 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from nutrition.models import DietaryPreference,MealPlan
-
-from orders.models import Orders, Order_items
-from payment.models import Transaction
-from .serializer import  Order_itemsSerializer, OrdersSerializer, ProductSerializer, ProductCategorySerializer, StockRecordSerializer, TransactionSerializer,DietaryPreferenceSerializer,MealPlanSerializer
-from products.models import Product, ProductCategory, StockRecord
-
-
+from .serializer import  DietaryPreferenceSerializer,MealPlanSerializer
 
 # Create your views here.
 class DietaryPreferenceViewSet(viewsets.ModelViewSet):
@@ -17,30 +11,3 @@ class DietaryPreferenceViewSet(viewsets.ModelViewSet):
 class MealPlanViewSet(viewsets.ModelViewSet):
     queryset = MealPlan.objects.all()
     serializer_class = MealPlanSerializer
-
-# Create your views here.
-class OrdersViewSet(viewsets.ModelViewSet):
-    queryset = Orders.objects.all()
-    serializer_class = OrdersSerializer
-
-class Order_itemsViewSet(viewsets.ModelViewSet):
-    queryset = Order_items.objects.all()
-    serializer_class =Order_itemsSerializer
-
-class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all()
-    serializer_class =ProductSerializer
-
-class ProductCategoryViewSet(viewsets.ModelViewSet):
-    queryset = ProductCategory.objects.all()
-    serializer_class =ProductCategorySerializer
-
-class StockRecordViewSet(viewsets.ModelViewSet):
-    queryset = StockRecord.objects.all()
-    serializer_class =StockRecordSerializer
-class TransactionViewSet(viewsets.ModelViewSet):
-      queryset = Transaction.objects.all()
-    serializer_class =TransactionSerializer
-
-
-
